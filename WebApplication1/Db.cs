b0bf59e38c20c399ab45961655a96571764ab297
kinko6 <85136008+kinko6@users.noreply.github.com>
2024-08-09 08:32:08 -0300

    Add files via upload

---
+++ WebApplication1/Db.cs
@@ -0,0 +1,52 @@
+﻿namespace WebApplication1.DB
+{
+    public record Ctx
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public static class CtxDB
+    {
+        public static List<Ctx> _ctx1 = new List<Ctx>()
+        {
+            new Ctx{Id=1, Name="ctx"}
+        };
+
+        public static List<Ctx> GetCtxStatic()
+        {
+            return _ctx1;
+        }
+
+        public static Ctx CreateCtx(Ctx ctx)
+        {
+            _ctx1.Add(ctx);
+            return ctx;
+        }
+
+        public static Ctx ? GetCtx(int id)
+        {
+            return _ctx1.SingleOrDefault(ctx => ctx.Id == id);
+        }
+
+        public static Ctx UpdateCtx(Ctx update)
+        {
+            _ctx1 = _ctx1.Select(ctx =>
+            {
+                if (ctx.Id == update.Id)
+                {
+                    ctx.Name = update.Name;
+                }
+                return ctx;
+            }).ToList();
+            return update;
+        }
+
+
+        public static void DeleteCtx(int id)
+        {
+            _ctx1 = _ctx1.FindAll(ctx => ctx.Id != id).ToList();
+        }
+
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.OpenApi.Models;
+using WebApplication1.DB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen(c => {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Simple API",
+            Description = "",
+            Version = "v1"
+        });
+    });
+}
+
+var app = builder.Build();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple API v1");
+});
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/ctx/{id}", (int id) => CtxDB.GetCtx(id));
+
+app.MapGet("/ctx", () => CtxDB.GetCtxStatic);
+
+app.MapPost("/animal", (Ctx ctx) => CtxDB.CreateCtx(ctx));
+
+app.MapPut("/animal", (Ctx ctx) => CtxDB.UpdateCtx(ctx));
+
+app.MapDelete("/animal", (int id) => CtxDB.DeleteCtx(id));
+
+app.Run();
